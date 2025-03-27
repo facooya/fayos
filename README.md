60d97e7aa7b6bdd42cc268d6ab9af4a24cec9d57
Fanone Facooya <facooya@gmail.com>
2025-03-27 11:21:18 +0900

    Cleanup: git add cli.s

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 FAYOS - FAcooYa Operating System
 
-boot/
-build/ - *.o, *.bin, *.img
-cmd/
-drivers/ - keyboard, disk
-kernel/ - kernel start (main loop)
-lib/ - common
+boot/  
+build/ - *.o, *.bin, *.img  
+cmd/  
+drivers/ - keyboard, disk  
+kernel/ - kernel start (main loop)  
+lib/ - common  
 
 Quick test for Linux x86-64 using qemu
 
--- kernel/cli.s
+++ kernel/cli.s
@@ -1,4 +1,5 @@
 # FAYOS - FAcooYa Operating System
+#
 # Copyright (C) 2025 Facooya
 # Copyright (C) 2025 Fanone Facooya
 #
@@ -15,35 +16,137 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# === > PREVIEW
-
-# FUNC
-# cli_tok()
-
-# DEPS
-# /src/inc/cmd/cli_buf.inc (
-#   FUNC: cli_buf_raw_set
-#   DATA: cli_buf_cmd, cli_buf_opt, cli_buf_arg
-# )
-
-# === < PREVIEW
-# === > CODE
-
 .code16
 .section .text
 
-.global cli_tok
+.global cmd_exec # exec_cli() !!!
+.global cli_buf_raw_set # set_cli_buf_raw() !!!
+.global cli_buf_init_all # init_cli_buf_init() !!!\
+.global cmd_table
+.global cli_buf_raw, cli_buf_cmd, cli_buf_arg
+.global cli_buf_opt, cli_buf_tmp, cli_buf_redir
 
-.extern cli_buf_raw_set # cli_buf.s
-.extern cli_buf_cmd, cli_buf_opt, cli_buf_arg # cli_buf.s
+# exec_cli()
+cmd_exec:
+  call cli_tok
 
-# -== > CLI Tokenizer
+  mov $cmd_table, %si
 
+_cmd_exec__chk_addr:
+  # Null ? not_found
+  mov (%si), %bx
+  test %bx, %bx
+  jz err_cmd # ref: err.inc
+
+_cmd_exec__cmp_cmd:
+  add $0x02, %si # cmd_table byte
+  mov $cli_buf_cmd, %di
+
+_cmd_exec__cmp_cmd_lp:
+  # Char != : next_lp
+  mov (%si), %al
+  cmp (%di), %al
+  jne _cmd_exec__next_lp
+
+  # Found
+  test %al, %al
+  jz _cmd_exec__call
+
+  # Loop
+  add $0x01, %si
+  add $0x01, %di
+  jmp _cmd_exec__cmp_cmd_lp
+
+_cmd_exec__next_lp:
+  # Char
+  mov (%si), %al
+  test %al, %al
+  jz _cmd_exec__next_end
+
+  # Loop
+  add $0x01, %si
+  jmp _cmd_exec__next_lp
+
+_cmd_exec__next_end:
+  add $0x01, %si
+  jmp _cmd_exec__chk_addr
+
+_cmd_exec__call:
+  call *%bx
+
+_cmd_exec__done:
+  ret
+
+# set_cli_buf_raw()
+cli_buf_raw_set:
+  mov $cli_buf_raw, %si
+  ret
+
+# init_cli_buf_all()
+cli_buf_init_all:
+  push $cli_buf_raw
+  call cli_buf_init
+  add $0x02, %sp
+
+  push $cli_buf_cmd
+  call cli_buf_init
+  add $0x02, %sp
+
+  push $cli_buf_arg
+  call cli_buf_init
+  add $0x02, %sp
+
+  push $cli_buf_opt
+  call cli_buf_init
+  add $0x02, %sp
+
+  push $cli_buf_tmp
+  call cli_buf_init
+  add $0x02, %sp
+
+  push $cli_buf_redir
+  call cli_buf_init
+  add $0x02, %sp
+
+  ret
+
+# init_cli_buf(cli_buf)
+cli_buf_init:
+_cli_buf_init__prol:
+  push %bp
+  mov %sp, %bp
+  push %si
+  push %ax
+
+  mov 4(%bp), %si
+
+_cli_buf_init__lp:
+  # Cond: null ? end
+  mov (%si), %al
+  test %al, %al
+  jz _cli_buf_init__end
+
+  # Init
+  xor %al, %al
+  movb %al, (%si)
+
+  # Loop: SI++
+  add $0x01, %si
+  jmp _cli_buf_init__lp
+
+_cli_buf_init__end:
+_cli_buf_init__epil:
+  pop %ax
+  pop %si
+  pop %bp
+
+_cli_buf_init__done:
+  ret
+
+# tok_cli_buf()
 cli_tok:
 _cli_tok__prol:
   call cli_buf_raw_set
-
-# --= > Buffer Command
 
 _cli_tok__buf_cmd_set:
   mov $cli_buf_cmd, %di
@@ -76,10 +179,6 @@ _cli_tok__buf_cmd_end:
   jne _cli_tok__buf_opt_end
 
   # Else: buf_opt_set
-
-# --= < Buffer Command
-# ===
-# --= > Buffer Option
 
 _cli_tok__buf_opt_set:
   # SI: Hyphen-Minus, SI+1: Option
@@ -122,10 +221,6 @@ _cli_tok__buf_opt_chk:
 _cli_tok__buf_opt_end:
   # SI: argument
 
-# --= < Buffer Option
-# ===
-# --= > Buffer Argument
-
 _cli_tok__buf_arg_set:
   mov $cli_buf_arg, %di
 
@@ -146,8 +241,6 @@ _cli_tok__buf_arg_lp:
 _cli_tok__buf_arg_end:
   # SI: null
 
-# --= < Buffer Argument
-
 _cli_tok__epli:
 _cli_tok__done:
   ret
@@ -164,6 +257,45 @@ _cli_tok__buf_opt_exit: # !!! Temporary
 
   ret
 
-# -== < CLI Tokenizer
-# ===
-# === < CODE
+err_cmd: # !!! err_cmd => cli.s
+  call newline # kbd.inc !!! Delete
+
+  # Print
+  push $.cmd_err_msg
+  call print_str # print.inc
+  add $0x02, %sp
+
+  call newline # kbd.inc !!! Delete
+  ret
+
+.section .data
+
+# cmd_table
+cmd_table:
+  .word cmd_clear
+  .asciz "clear"
+  .word cmd_echo
+  .asciz "echo"
+  .word cmd_touch
+  .asciz "touch"
+  .word cmd_rm
+  .asciz "rm"
+  .word cmd_ls
+  .asciz "ls"
+  .word cmd_cat
+  .asciz "cat"
+  .word cmd_help
+  .asciz "help"
+  .word 0x00
+  .asciz ""
+
+# cli buffers
+cli_buf_raw: .zero 0x40
+cli_buf_cmd: .zero 0x20
+cli_buf_arg: .zero 0x20
+cli_buf_opt: .zero 0x10
+cli_buf_tmp: .zero 0x20
+cli_buf_redir: .zero 0x20
+
+# err
+.cmd_err_msg: .asciz "Command not found. Try \"help\" for a list of commands."
