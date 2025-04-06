f29177cc5b8350065abe4e9f01242fce34150dae
Fanone Facooya <facooya@gmail.com>
2025-04-07 03:09:11 +0900

    docs: debug note [d_nsh]

--- io/kbd.s
+++ io/kbd.s
@@ -19,7 +19,7 @@
 # hdl_kbd()
 #
 # .hdl_ins()
-# .hdl_bs_del()
+# .hdl_bs_lsh()
 # .hdl_bs
 # .hdl_enter
 # .hdl_left
@@ -31,7 +31,7 @@
 # .hdl_ins()
 #   print_str
 #
-# .hdl_bs_del()
+# .hdl_bs_lsh()
 #   kernel_min_cur_pos_x
 #   kernel_max_cur_pos_x
 #   print_str
@@ -50,6 +50,45 @@
 #     0x4D - right
 #     0x48 - up
 #     0x50 - down
+#
+# [d_nsh]
+#   c: cursor, si: buf_raw ptr, dl: cursor x
+#     [0]: null (buf), [ ]: space (screen)
+#     {c,si,dl}: idx
+#
+#   <buf> facooya{si}[0]
+#   <screen> facooya{c,dl}[ ]
+#
+#   [d_nsh.1] # back cursor
+#     <screen> facooya{c,dl}[ ]
+#       (c--)
+#       ah=set;
+#       dl--; facooy{dl}a{c}[ ]
+#       int; facooy{c,dl}a[ ]
+#
+#   [d_nsh.2] # overwrite
+#     <screen> facooy{c,dl}a[ ]
+#       (c++)
+#       ah=out;
+#       al=[ ];
+#       int; facooy{dl}[ ]{c}[ ]
+#
+#   [d_nsh.3] # back cursor
+#     <screen> facooy{dl}[ ]{c}[ ]
+#       (c=dl)
+#       ah=set;
+#       int; facooy{c,dl}[ ][ ]
+#
+#   [d_nsh.4] # ptr, buf
+#     <buf> facooya{si}[0]
+#       si--; facooy{si}a[0]
+#       (si)=[0]; facooy{si}[0][0]
+#
+#   <buf> facooy{si}[0]
+#   <screen> facooy{c}[ ]
+#
+# [d_lsh]
+# 
 
 .code16
 .section .text
@@ -199,57 +238,55 @@ hdl_kbd:
 
 # .hdl_bs
 .hdl_bs:
-  # get cursor
+  # get {cur}
   mov $0x03, %ah
   mov $0x00, %bh
   int $0x10
 
-  # try back cursor
-  mov $0x02, %ah
-
-  # reg_dl = x (current)
-  # cond: max ? done
-  cmp (kernel_min_cur_pos_x), %dl
+  # cond: min ? done {cur}
+  cmp (kernel_min_cur_pos_x), %dl # dl: cur pos x
   je .hdl_kbd__done
 
-  # Debug "." = cursor, "," = si
-  # back cursor !!! hel.,lo
-  sub $0x01, %dl
-  int $0x10 # x-- !!! he.l,lo
-
-  # overwrite
-  mov $0x0E, %ah
-  mov $0x20, %al # space
-  int $0x10 # x++ !!! he .,lo
-
-  # back cursor (again)
-  mov $0x02, %ah
-  int $0x10 # x-- !!! he. ,lo
-
-  # max cursor
+  # dec max {cur}
   mov (kernel_max_cur_pos_x), %al
   sub $0x01, %al
   mov %al, (kernel_max_cur_pos_x)
 
-  # cond: null != ? call_del
-  mov (%si), %al # si = buf_raw + offset !!! he. ,lo
+  # cond: null != ? call_lsh {lsh}
+  mov (%si), %al # si: buf_raw ptr
   test %al, %al
-  jnz .hdl_bs__call_del
+  jnz .hdl_bs__call_lsh
 
-  sub $0x01, %si # next origin
+  # default {nsh} [d_nsh]
+  # back cursor {cur,nsh} [d_nsh.1]
+  mov $0x02, %ah
+  sub $0x01, %dl
+  int $0x10
+
+  # overwrite {cur,nsh} [d_nsh.2]
+  mov $0x0E, %ah
+  mov $0x20, %al # space
+  int $0x10
+
+  # back cursor {cur,nsh} [d_nsh.3]
+  mov $0x02, %ah
+  int $0x10
+
+  # ptr, buf {nsh} [d_nsh.4]
+  sub $0x01, %si
   movb $0x00, (%si)
 
   jmp .hdl_kbd__done
 
-.hdl_bs__call_del:
-  push %si # origin !!! he ,lo
-  call .hdl_bs_del
+.hdl_bs__call_lsh:
+  push %si # origin !!! facoo{c,si}ya[0]
+  call .hdl_bs_lsh
   add $0x02, %sp
 
   jmp .hdl_kbd__done
 
-# .hdl_bs_del()
-.hdl_bs_del:
+# .hdl_bs_lsh()
+.hdl_bs_lsh:
   # prol
   push %bp
   mov %sp, %bp
@@ -259,42 +296,53 @@ hdl_kbd:
   push %cx
   push %dx
 
-  mov 4(%bp), %di # set origin !!! he ,lo
+  mov 4(%bp), %di # set origin !!! facoo{c,si,di}ya[0]
 
-  # get cursor !!! he. lo
+  # get cursor
   mov $0x03, %ah
   mov $0x00, %bh
   int $0x10
 
-.hdl_bs_del__lsh_lp:
-  # left shift !!! he ,lo[0] => helo[0],
+.hdl_bs_lsh__lp:
+  # !!! buf: facoo{si,di}ya[0], screen: facoo{c}ya[ ]
+  # left shift
   mov (%di), %al
   mov %al, -1(%di)
 
-  # cond: null ? lsh_end
+  # cond: null ? end
   mov (%di), %al
   test %al, %al
-  jz .hdl_bs_del__lsh_end
+  jz .hdl_bs_lsh__end
 
   # loop
   add $0x01, %di
-  jmp .hdl_bs_del__lsh_lp
+  jmp .hdl_bs_lsh__lp
 
-.hdl_bs_del__lsh_end:
-  # screen delete !!! helo[0], => helo[space],
-  mov $0x20, %al # space
-  mov %al, -1(%di)
+.hdl_bs_lsh__end:
+  # !!! buf: facoy{si}a[0]{di}[0]
+  # !!! buf: facoy{si}a[0], screen: facoo{c}ya[ ]
 
-  sub $0x01, %si # next origin !!! si = 2, he[si]lo[space]
+  sub $0x01, %si # next origin !!! buf: faco{si}ya[0]
 
-  push %si # origin !!! he[cur]lo[space] => lo[space][cur]
+  # set cursor
+  sub $0x01, %dl
+  mov $0x02, %ah
+  mov $0x00, %bh
+  int $0x10 # !!! screen: faco{c}oya[ ]
+
+  push %si # origin !!! screen: faco{c}oya[ ] => print: {si}ya{c}, screen: facoya{c}a[ ]
   call print_str
   add $0x02, %sp
 
-  # set cursor !!! helo(1)[c] => he[c]lo[1][0]
+  # overwrite
+  mov $0x0E, %ah
+  mov $0x20, %al # space
+  int $0x10 # !!! screen: faco{si}oy[ ]{c}[ ]
+
+  # set cursor
   mov $0x02, %ah
   mov $0x00, %bh
-  int $0x10
+  int $0x10 # !!! screen: faco{c,si}ya{c}[ ][ ]
 
   # epil
   pop %dx
