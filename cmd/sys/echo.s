4f12add832e5f9993b4f058086a1fad77eda7662
Fanone Facooya <facooya@gmail.com>
2025-06-01 17:40:36 +0900

    refactor(echo): apply new args-system

--- cmd/sys/echo.s
+++ cmd/sys/echo.s
@@ -50,10 +50,12 @@ cmd_echo:
 	# {init}
 	mov %bx, %ax # optc
 	xor %bx, %bx # opt_flag
+	add $0x02, %di # skip argv[0] (cmd)
+	sub $0x01, %cx # skip argv[0]
 
 	# {task} (opt_c == 0)
 	test %ax, %ax
-	jz .arg
+	jz .run
 	jmp .opt
 
 # <PRE>
@@ -61,13 +63,12 @@ cmd_echo:
 .opt:
 	mov %ax, %dx # opt_c
 
-	add $0x02, %di # skip argv[0] (cmd)
 	add (%di), %si # buf += argv[1]
 	add $0x01, %si # skip hyphen
 
 # <PRE>
-# *si == opt_chr
-.opt__lp_chk:
+# (*si == opt_chr)
+.opt__lp:
 	mov (%si), %al
 
 	# (opt_chr == e)
@@ -83,131 +84,111 @@ cmd_echo:
 
 .opt__set_e:
 	bts $0x00, %bx
-	jmp .opt__chk
+	jmp .opt__set_chk
 
 .opt__set_n:
 	bts $0x01, %bx
-	jmp .opt__chk
+	jmp .opt__set_chk
 
-.opt__chk:
-.opt__set_end:
+.opt__set_chk:
 	add $0x01, %si
 
 	# {end.step} (opt_chr == null)
 	mov (%si), %al
 	test %al, %al
-	jz .opt__end
+	jz .opt__chk
 
-	jmp .opt__lp_chk
+	# {lp}
+	jmp .opt__lp
 
 # <PRE>
-# *si == null
-.opt__end:
-	# {task} (opt_c == 0)
-	sub $0x01, %cx
-	test %cx, %cx
-	jz .arg
-
+# (*si == null)
+.opt__chk:
 	# {step}
-	add $0x02, %si # skip null+hyphen
-	jmp .opt__lp_chk
+	sub $0x01, %cx # argc
+	sub $0x01, %dx # optc
 
-.arg:
-.arg__init:
-	# set offset {init}
+	# {end} (optc == 0)
+	test %dx, %dx
+	jz .opt__end
+
+	# {lp}
+	add $0x02, %si # skip null+hyphen
+	add $0x02, %di # argv[n+1]
+	jmp .opt__lp
+
+# <PRE>
+# (*si == null)
+.opt__end:
+	add $0x02, %di # argv[n+1]
+
+	# {task}
+	jmp .run
+
+# {TASK}
+# <PRE>
+# (*di == arg_idx)
+.run:
+	# {init}
 	mov $raw_buf, %si
 	add $0x02, %si
 
-	mov $args_info, %di
-	mov 0x04(%di), %cx # arg_c
-	mov 0x06(%di), %ax # arg_idx
+	mov (%di), %ax # arg_idx
 	add %ax, %si
 
-	# {task}
 	call outnl
-	jmp .run
-
-.run:
-.run__init:
-	push %bx # opt_flag
-
-	mov $args_info, %di
-	mov (%di), %bx # opt_c
-	mov 0x04(%di), %cx # arg_c
-
-	mov $args, %di
-	add $0x02, %di # skip argc
-
-	xor %dx, %dx
-	mov $0x02, %ax # align
-	mul %bx # opt_c
-	add %ax, %di # *di = arg_idx
-
-	mov $raw_buf, %si
-	add $0x02, %si # skip len
-
-	mov (%di), %ax
-	add %ax, %si
-
-	pop %bx # opt_flag
-
-	add $0x30, %al
-	call sys_tty_out
 
 .run__lp:
-	# (opt == e)
+	# {step} (opt == e)
 	bt $0x00, %bx
-	jc .run__opt_e
+	jc .run__exec_e
 
-	# default
 	push %si
 	call print_str
 	add $0x02, %sp
 
-	# jmp
-	jmp .run__skip_opt_e
+	# {step}
+	jmp .run__skip_e
 
-.run__opt_e:
+.run__exec_e:
 	push %si
 	call print_esc
 	add $0x02, %sp
 
-.run__skip_opt_e:
-	# (opt == n)
+.run__skip_e:
+	# {step.skip} (opt != n)
 	bt $0x1, %bx
-	jc .run__opt_n
+	jnc .run__skip_n
 
-	# default
-	call outsp
-
-	# {end}
-	jmp .run__end
-
-.run__opt_n:
-	# {end.done} (arg_c == last)
+	# {end} (argc == last)
 	cmp $0x01, %cx
-	je .done
+	je .run__end
 
-	# {end}
+.run__skip_n:
 	call outsp
-	jmp .run__end
 
-.run__end:
-	sub $0x01, %cx # arg_c
+.run__chk:
+	sub $0x01, %cx # argc
 
-	# {end.done} (arg_c == 0)
+	# {end} (arg_c == 0)
 	test %cx, %cx
-	jz .nl_done
+	jz .run__end
 
+	# {lp.init}
 	mov $raw_buf, %si
 	add $0x02, %si # skip len
 
-	add $0x02, %di
+	add $0x02, %di # argv[n+1]
 	mov (%di), %ax
 	add %ax, %si
 
-	# {step}
+	# {lp}
 	jmp .run__lp
+
+.run__end:
+	test %cx, %cx
+	jz .nl_done
+	jmp .done
 
 # DONE
 .nl_done:
