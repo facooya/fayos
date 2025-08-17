2ca4bb214346a7f3f944391be0c499ea6b49cc80
Fanone Facooya <facooya@gmail.com>
2025-08-17 15:07:38 +0900

    chore: remove clear_buf

--- kernel/io/buf.s
+++ kernel/io/buf.s
@@ -21,44 +21,7 @@ path_buf: .zero 0x100
 
 .section .text
 .code16
-.global clear_buf
 .global clear_redir_buf
-
-# clear_buf(buf)
-clear_buf:
-	# prol
-	push %bp
-	mov %sp, %bp
-	push %si
-	
-	# init
-	mov 0x04(%bp), %si
-	mov (%si), %cx
-
-	# zero_len
-	xor %ax, %ax
-	mov %ax, (%si)
-	add $0x02, %si
-
-.clear_buf__zero_buf:
-	# {escape}
-	test %cx, %cx
-	jz .clear_buf__done
-
-	# zero
-	xor %al, %al
-	mov %al, (%si)
-
-	# step
-	add $0x01, %si
-	sub $0x01, %cx
-	jmp .clear_buf__zero_buf
-
-.clear_buf__done:
-	# epil
-	pop %si
-	pop %bp
-	ret
 
 # clear_redir_buf()
 clear_redir_buf:
