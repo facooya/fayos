2463e14dce3c34f54a719b16181f504e54ea4e65
Facooya <facooya@gmail.com>
2025-11-04 09:58:19 +0900

    refactor(cmd/rmdir): apply fs_rm()

--- sh/cmd/dir/rmdir.s
+++ sh/cmd/dir/rmdir.s
@@ -11,6 +11,7 @@
 .code16
 .global cmd_rmdir
 
+# TODO: not found, not dir !!!!!!!!!!!!!
 # cmd_rmdir()
 cmd_rmdir:
 	push %es
@@ -69,64 +70,25 @@ cmd_rmdir:
 	add $0x02, %si
 	add %ax, %si
 
-	# { TEST
+	push %si # (&name)
+	push $fsp+FSP_OFF_DIR # (fsp &src)
+	call de_seek
+	add $0x04, %sp
+	# <ax = {true:off, false:1}>
+	add %ax, %bx
+
+	# (f_type != dir) ? {err}
+	mov %es:DE_OFF_FILE_TYPE(%bx), %al
+	cmp $0x40, %al
+	jne .err_dir_type
+
 	push %si # (&name)
 	push $fsp+FSP_OFF_DIR # (fsp &src)
 	call fs_rm
 	add $0x04, %sp
-	# }
 	jmp .done
 
-	push %si # (&name)
-	push $fsp+FSP_OFF_DIR # (fsp &dst)
-	call de_seek
-	add $0x04, %sp
-	# <ax = {true:off, false:1}>
-	add %ax, %bx
-
-	# (file_type != dir) ? {err} : {run.lp}
-	mov %es:DE_OFF_FILE_TYPE(%bx), %al
-	cmp $0x40, %al
-	jne .err_dir_type
-
-	mov %es:DE_OFF_INUM(%bx), %ax
-	mov %ax, (rmdir_inum)
-	mov %es:DE_OFF_INUM+0x02(%bx), %ax
-	mov %ax, (rmdir_inum+0x02)
-
-.run__lp_p:
-	push $rmdir_inum
-	call get_bottom_dir
-	add $0x02, %sp
-	# <ret> tmp_inum
-
-	push $tmp_inum
-	call rm_dir
-	add $0x02, %sp
-
-	# (rmdir_inum != tmp_inum) ? {lp}
-	mov (tmp_inum), %ax
-	mov (rmdir_inum), %dx
-	cmp %ax, %dx
-	jne .run__lp_p
-	mov (tmp_inum+0x02), %ax
-	mov (rmdir_inum+0x02), %dx
-	cmp %ax, %dx
-	jne .run__lp_p
-
-	mov $path_cv, %si
-	mov (%si), %cx # pathc
-	add %cx, %si
-	add %cx, %si # pathv[last]
-
-	mov (%si), %ax
-	mov $path_sbuf, %si
-	add $0x02, %si
-	add %ax, %si
-	jmp .run__end_path
-
 .path_pass:
-	# {{{ de_seek
 	push $fsp+FSP_OFF_CUR # (fsp &src)
 	call disk_read_fsp
 	add $0x02, %sp
@@ -139,124 +101,21 @@ cmd_rmdir:
 	call de_seek
 	add $0x04, %sp
 	# <ax = {true:off, false:1}>
-	add %ax, %bx
-
-	# (de_seek() == no_match) ? {err}
-	cmp $0x01, %ax
-	je .err_dir_no
-
-	xor %ax, %ax
-	push $FSP_SIZE # (size)
-	push $fsp+FSP_OFF_CUR # (&s_off)
-	push %ax # (&s_seg)
-	push $fsp+FSP_OFF_DIR # (&d_off)
-	push %ax # (&d_seg)
-	call mem_cpy
-	add $0x0A, %sp
-
-	mov $args, %si
-	mov 0x06(%si), %ax # argv[1]
-	mov $cl_lbuf, %si
-	add $0x02, %si
-	add %ax, %si # cl_lbuf[argv[1]]
-
-	# (arg == dots) ? {err}
-	mov (%si), %ax
-	cmp $0x2E2E, %ax
-	je .err_inv_arg
-
-	# (arg == dot) ? {err}
-	cmp $0x002E, %ax
-	je .err_inv_arg
-	# }}}
-
-.chk_err:
-	# (file_type != dir) ? {err} : {run}
-	mov %es:DE_OFF_FILE_TYPE(%bx), %al
-	cmp $0x40, %al
-	jne .err_dir_type
-	jmp .run
-
-.run:
-	mov %es:DE_OFF_INUM(%bx), %ax
-	mov %ax, (rmdir_inum)
-	mov %es:DE_OFF_INUM+0x02(%bx), %ax
-	mov %ax, (rmdir_inum+0x02)
-
-.run__lp:
-	push $rmdir_inum
-	call get_bottom_dir
-	add $0x02, %sp
-	# <ret> tmp_inum
-
-	push $tmp_inum
-	call rm_dir
-	add $0x02, %sp
-
-	# {lp} (rmdir_inum != tmp_inum)
-	mov (tmp_inum), %ax
-	mov (rmdir_inum), %dx
-	cmp %ax, %dx
-	jne .run__lp
-	mov (tmp_inum+0x02), %ax
-	mov (rmdir_inum+0x02), %dx
-	cmp %ax, %dx
-	jne .run__lp
-
-	# {end}
-	jmp .run__end
-
-.run__end:
-	# {{{ last remove
-	mov $args, %si
-	mov 0x06(%si), %ax # argv[1]
-	mov $cl_lbuf, %si
-	add $0x02, %si
-	add %ax, %si # cl_lbuf[argv[1]]
-
-	mov (inum), %ax
-	mov %ax, (parent_path_inum)
-	mov (inum+0x02), %ax
-	mov %ax, (parent_path_inum+0x02)
-
-.run__end_path:
-	push $fsp+FSP_OFF_DIR # (fsp &src)
-	call disk_read_fsp
-	add $0x02, %sp
-	# <dx:ax = seg:off>
-	mov %dx, %es
-	mov %ax, %bx
-
-	push %si # (&name)
-	push $fsp+FSP_OFF_DIR # (fsp &src)
-	call de_seek
-	add $0x04, %sp
-	# <ax = {true:off, false:1}>
-	add %ax, %bx
-
+	
 	# (de_seek() == false) ? {err}
 	cmp $0x01, %ax
-	je .err_inv_path
+	je .err_dir_no
+	add %ax, %bx
 
-	mov %es:DE_OFF_INUM(%bx), %ax
-	mov %ax, (clear_inum)
-	mov %es:DE_OFF_INUM+0x02(%bx), %ax
-	mov %ax, (clear_inum+0x02)
+	# (f_type != dir) ? {err}
+	mov %es:DE_OFF_FILE_TYPE(%bx), %al
+	cmp $0x40, %al
+	jne .err_dir_type
 
-	# clear inum
-	xor %ax, %ax
-	mov %ax, %es:DE_OFF_INUM(%bx)
-	mov %ax, %es:DE_OFF_INUM+0x02(%bx)
-
-	push $fsp+FSP_OFF_DIR # (fsp &src)
-	call disk_write_fsp
-	add $0x02, %sp
-
-	push (clear_inum)
-	push (clear_inum+0x02)
-	call ind_clr
+	push %si # (&name)
+	push $fsp+FSP_OFF_CUR # (fsp &src)
+	call fs_rm
 	add $0x04, %sp
-
 	jmp .done
 
 # {DONE}
