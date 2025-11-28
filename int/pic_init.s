89bdafd48c02556b819a138cb0bac2029b7b7cdd
Facooya <facooya@gmail.com>
2025-11-29 00:27:56 +0900

    refactor(pic_init): update constants

--- int/pic_init.s
+++ int/pic_init.s
@@ -15,13 +15,13 @@
 # pic_init()
 pic_init:
 	# init
-	mov $(ICW1_INIT|ICW1_ICW4), %al # ICW1_INIT+ICW1_ICW4
+	mov $(ICW1_INIT|ICW1_ICW4), %al
 	out %al, $PIC1_PORT_CMD
 	out %al, $IO_WAIT
 	out %al, $PIC2_PORT_CMD
 	out %al, $IO_WAIT
 
-	# IRQ remap 0x20-0x2F
+	# irq remap
 	mov $ICW2_PIC1, %al
 	out %al, $PIC1_PORT_DATA
 	out %al, $IO_WAIT
@@ -30,36 +30,40 @@ pic_init:
 	out %al, $IO_WAIT
 
 	# connect
-	mov $ICW3_PIC1, %al
+	mov $ICW3_PIC1_PIC2, %al
 	out %al, $PIC1_PORT_DATA
 	out %al, $IO_WAIT
-	mov $ICW3_PIC2, %al
+	mov $ICW3_PIC2_ID, %al
 	out %al, $PIC2_PORT_DATA
 	out %al, $IO_WAIT
 
+	# cpu mode
 	mov $ICW4_8086, %al
 	out %al, $PIC1_PORT_DATA
 	out %al, $IO_WAIT
 	out %al, $PIC2_PORT_DATA
 	out %al, $IO_WAIT
 
-	mov $0xFF, %al
+	# { ocw 1
+	# disable irq all
+	mov $IMR_INIT, %al
 	out %al, $PIC1_PORT_DATA
 	out %al, $PIC2_PORT_DATA
 
 	# enable irq 1
 	in $PIC1_PORT_DATA, %al
-	and $0xFD, %al
+	and $~IMR_BIT_IRQ1, %al
 	out %al, $PIC1_PORT_DATA
 
-	# { enable irq 8
-	in $PIC2_PORT_DATA, %al
-	and $~(0x01<<0x00), %al
-	out %al, $PIC2_PORT_DATA
-
+	# enable irq 2
 	in $PIC1_PORT_DATA, %al
-	and $~(0x01<<0x02), %al
+	and $~IMR_BIT_IRQ2, %al
 	out %al, $PIC1_PORT_DATA
+
+	# enable irq 8
+	in $PIC2_PORT_DATA, %al
+	and $~IMR_BIT_IRQ8, %al
+	out %al, $PIC2_PORT_DATA
 	# }
 
 	ret
